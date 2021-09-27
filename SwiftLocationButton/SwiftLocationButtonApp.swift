580aab3cc8ef0934b032477182311858ce591c43
Johan Guse <johanguse@gmail.com>
2021-09-27 19:27:01 -0300

    Initial Commit

---
+++ SwiftLocationButton/SwiftLocationButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLocationButtonApp.swift
+//  SwiftLocationButton
+//
+//  Created by Johan Guse on 27/09/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLocationButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
