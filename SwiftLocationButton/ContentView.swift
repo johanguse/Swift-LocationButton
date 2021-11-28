17823b461f659339e03f62a0048dddf1ac84501f
Johan Guse <johanguse@gmail.com>
2021-11-28 18:51:42 -0300

    Finish Swift Button Location

--- SwiftLocationButton/ContentView.swift
+++ SwiftLocationButton/ContentView.swift
@@ -5,17 +5,67 @@
 //  Created by Johan Guse on 27/09/21.
 //
 
+import CoreLocationUI
+import MapKit
 import SwiftUI
 
+
 struct ContentView: View {
+    
+    @StateObject private var viewModel = ContentViewModel()
+        
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack(alignment: .bottom) {
+            Map(coordinateRegion: $viewModel.region, showsUserLocation: true)
+                .ignoresSafeArea()
+            
+
+                LocationButton(.currentLocation) {
+                    viewModel.requestAllowOnceLocationPermission()
+                }
+                .foregroundColor(.white)
+                .cornerRadius(8)
+                .labelStyle(.titleAndIcon)
+                .symbolVariant(.fill)
+                .padding(.bottom, 80)
+                
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+final class ContentViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
+    
+    @Published var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 0, longitude: -70), span: MKCoordinateSpan(latitudeDelta: 100, longitudeDelta: 100))
+    
+    let locationManager = CLLocationManager()
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+    }
+    
+    func requestAllowOnceLocationPermission(){
+        locationManager.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let lastestLocation = locations.first else {
+            // error
+            return
+        }
+        
+        DispatchQueue.main.async {
+            self.region = MKCoordinateRegion(center: lastestLocation.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
     }
 }
